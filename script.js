e91cf89d3ce957f7cf318b0dbb1defaf9fac17a6
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-10-21 20:36:15 -0400

    e

--- script.js
+++ script.js
@@ -2,28 +2,37 @@ document.addEventListener("DOMContentLoaded", function () {
     const chatBox = document.getElementById("chat-box");
     const messageInput = document.getElementById("message");
     const sendButton = document.getElementById("send");
+    let userName = prompt("Please enter your name:"); // Prompt for the user's name
 
-    sendButton.addEventListener("click", sendMessage);
+    // Ensure the user provides a name
+    while (!userName) {
+        userName = prompt("Please enter a valid name:");
+    }
 
     function sendMessage() {
         const message = messageInput.value;
         if (message) {
-            appendMessage("You", message);
+            appendMessage(userName, message, true); // Use the user's provided name
             messageInput.value = "";
 
             // Send the message to the server
-            sendToServer(message);
+            sendToServer(message, userName); // Pass the sender's name
         }
     }
 
-    function appendMessage(sender, message) {
+    function appendMessage(sender, message, isUserMessage) {
         const messageElement = document.createElement("p");
-        messageElement.textContent = `${sender}: ${message}`;
+        if (isUserMessage) {
+            // Bold the name for user messages
+            messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+        } else {
+            messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+        }
         chatBox.appendChild(messageElement);
         chatBox.scrollTop = chatBox.scrollHeight;
     }
 
-    function sendToServer(message) {
+    function sendToServer(message, sender) {
         // Simulated server logic (using PHP)
         // In a real application, you would make an AJAX request or use WebSockets.
         const xhr = new XMLHttpRequest();
@@ -35,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (response.indexOf("Message sent:") === 0) {
                     // Only append the message to the chat if it starts with "Message sent:"
                     const message = response.replace("Message sent:", "").trim();
-                    appendMessage("You", message);
+                    appendMessage(sender, message, true); // Bold the sender's name
                 }
             }
         };
@@ -54,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 messages.forEach((msg) => {
                     const [sender, message] = msg.split(":");
                     if (sender && message) {
-                        appendMessage(sender, message);
+                        appendMessage(sender, message, false); // Don't bold the sender's name
                     }
                 });
             }
