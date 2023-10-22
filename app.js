382223dfd6fa9c7aa6139511707216071fadf1ce
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-10-21 21:03:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,79 +1,81 @@
-// @ts-nocheck
-
-const johnSelectorBtn = document.querySelector('#john-selector')
-const janeSelectorBtn = document.querySelector('#jane-selector')
-const chatHeader = document.querySelector('.chat-header')
-const chatMessages = document.querySelector('.chat-messages')
-const chatInputForm = document.querySelector('.chat-input-form')
-const chatInput = document.querySelector('.chat-input')
-const clearChatBtn = document.querySelector('.clear-chat-button')
-
-const messages = JSON.parse(localStorage.getItem('messages')) || []
-
-const createChatMessageElement = (message) => `
-  <div class="message ${message.sender === 'John' ? 'blue-bg' : 'gray-bg'}">
-    <div class="message-sender">${message.sender}</div>
-    <div class="message-text">${message.text}</div>
-    <div class="message-timestamp">${message.timestamp}</div>
-  </div>
-`
-
-window.onload = () => {
-  messages.forEach((message) => {
-    chatMessages.innerHTML += createChatMessageElement(message)
-  })
-}
-
-let messageSender = 'John'
-
-const updateMessageSender = (name) => {
-  messageSender = name
-  chatHeader.innerText = `${messageSender} chatting...`
-  chatInput.placeholder = `Type here, ${messageSender}...`
-
-  if (name === 'John') {
-    johnSelectorBtn.classList.add('active-person')
-    janeSelectorBtn.classList.remove('active-person')
-  }
-  if (name === 'Jane') {
-    janeSelectorBtn.classList.add('active-person')
-    johnSelectorBtn.classList.remove('active-person')
-  }
-
-  /* auto-focus the input field */
-  chatInput.focus()
-}
-
-johnSelectorBtn.onclick = () => updateMessageSender('John')
-janeSelectorBtn.onclick = () => updateMessageSender('Jane')
-
-const sendMessage = (e) => {
-  e.preventDefault()
-
-  const timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-  const message = {
-    sender: messageSender,
-    text: chatInput.value,
-    timestamp,
-  }
-
-  /* Save message to local storage */
-  messages.push(message)
-  localStorage.setItem('messages', JSON.stringify(messages))
-
-  /* Add message to DOM */
-  chatMessages.innerHTML += createChatMessageElement(message)
-
-  /* Clear input field */
-  chatInputForm.reset()
-
-  /*  Scroll to bottom of chat messages */
-  chatMessages.scrollTop = chatMessages.scrollHeight
-}
-
-chatInputForm.addEventListener('submit', sendMessage)
-
-clearChatBtn.addEventListener('click', () => {
-  localStorage.clear()
-  chatMessages.innerHTML = ''
-})
+$(document).ready(function() {
+    // Array to store chat messages
+    let chatMessages = [];
+ 
+    // Function to display chat messages
+    function displayChatMessages() {
+        let chatMessagesHtml = '';
+        chatMessages.forEach(function(message) {
+            chatMessagesHtml += '<div class="chat-message"><span class="sender">' + message.sender + ':</span><div class="message">' + message.message + '</div></div>';
+        });
+        $('#chatMessages').html(chatMessagesHtml);
+    }
+ 
+    // Function to send a message
+    function sendMessage() {
+        let messageInput = $('#messageInput');
+        let message = messageInput.val().trim();
+        if (message !== '') {
+            let newMessage = {
+                sender: 'User',
+                message: message
+            };
+            chatMessages.push(newMessage);
+            displayChatMessages();
+            messageInput.val('');
+        }
+    }
+ 
+    // Event listener for send button click
+    $('#sendButton').click(function() {
+        sendMessage();
+    });
+ 
+    // Event listener for enter key press
+    $('#messageInput').keypress(function(e) {
+        if (e.which === 13) {
+            sendMessage();
+        }
+    });
+});
+ 
+        $(document).ready(function() {
+            // Array to store chat messages
+            let chatMessages = [];
+ 
+            // Function to display chat messages
+            function displayChatMessages() {
+                let chatMessagesHtml = '';
+                chatMessages.forEach(function(message) {
+                    chatMessagesHtml += '' + message.sender + ':' + message.message + '';
+                });
+                $('#chatMessages').html(chatMessagesHtml);
+            }
+ 
+            // Function to send a message
+            function sendMessage() {
+                let messageInput = $('#messageInput');
+                let message = messageInput.val().trim();
+                if (message !== '') {
+                    let newMessage = {
+                        sender: 'User',
+                        message: message
+                    };
+                    chatMessages.push(newMessage);
+                    displayChatMessages();
+                    messageInput.val('');
+                }
+            }
+ 
+            // Event listener for send button click
+            $('#sendButton').click(function() {
+                sendMessage();
+            });
+ 
+            // Event listener for enter key press
+            $('#messageInput').keypress(function(e) {
+                if (e.which === 13) {
+                    sendMessage();
+                }
+            });
+        });
